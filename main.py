fdafb5e95010d9b3d04b159c689b7c92dc869b90
jjpanpg <phua.jj@pg.com>
2023-05-24 13:54:21 +0200

    update main

--- main.py
+++ main.py
@@ -5,28 +5,28 @@ import streamlit as st
 
 st.title("YOLO Coder QnA 🔎")
 
+if 'is_post' not in st.session_state:
+    st.session_state['is_post'] = True
 
 # Form
-form = st.form("api_form")
 
-base_url = form.text_input("Base URL", "https://aiehackathon2023.replit.app")
-option = form.selectbox(
+
+base_url = st.text_input("Base URL", "https://aiehackathon2023.replit.app")
+option = st.selectbox(
         "Endpoints",
         ("analysis", "supplier", "product", "order"),
     )
-http_method = form.radio(
+http_method = st.radio(
         "HTTP method 👉",
         key="disabled",
         options=["GET", "POST"]
     )
 
-payload = form.text_area('JSON Payload', '''
-    
-    ''')
-
+if http_method == 'POST':
+    payload = st.text_area('JSON Payload', "", height=None)
 
 # query = form.text_input(f"{base_url} {option}")
-
+form = st.form("api_form")
 
 submit = form.form_submit_button("Submit")
 
@@ -36,28 +36,17 @@ if submit:
     url = f"{base_url}/{option}"
 
     print(option)
-    payload = ""
     if http_method == 'GET':
         payload = ""
         response = requests.get(url, headers=headers)
         # print(response.json())
 
     if http_method == 'POST':
-        response = requests.post(url, data=json.dumps(payload), headers=headers)
+
+        payload = " ".join(payload.split())
+        json_file = json.dumps(payload)
+        print(json_file)
+        response = requests.post(url, data=json_file, headers=headers)
 
     st.json(response.json(), expanded=True)
-    # text = st.text_area("HTTP Response", str(response.json()))
 
-
-# form = st.form("my_form")
-# input_form = form.text_area("Enter your questions")
-# form.form_submit_button("Submit")
-
-
-
-# start_phrase = 'Write a tagline for an ice cream shop. '
-# response = openai.Completion.create(engine=deployment_name, prompt=start_phrase, max_tokens=10)
-# text = response['choices'][0]['text'].replace('\n', '').replace(' .', '.').strip()
-
-# # print the response
-# print(response['choices'][0]['message']['content'])
